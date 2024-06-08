e01fe333cf3dbee0ab2f88858b68404107897e62
Sebastian Usma <johan58usma@gmail.com>
2024-06-08 01:04:08 -0500

    Insertar nuevo videojuego funcional

--- 13-videojuegosAPI/API/actualizarVideojuego.php
+++ 13-videojuegosAPI/API/actualizarVideojuego.php
@@ -1,33 +1,22 @@
 <?php
     include "../DB/conexion.php";
 
-    // Verificar si se han enviado todos los datos necesarios
     if(!empty($_POST["id_juego"]) && !empty($_POST["nombre_videojuego"]) && !empty($_POST["descripcion_videojuego"]) && !empty($_POST["url_portada"])) {
         $nombre_videojuego = $_POST["nombre_videojuego"];
         $descripcion_videojuego = $_POST["descripcion_videojuego"];
         $url_portada = $_POST["url_portada"];
         $id_juego = $_POST["id_juego"];
 
-        // Imprimir los valores recibidos para verificar
-        echo "id_juego: " . $id_juego . "\n";
-        echo "nombre_videojuego: " . $nombre_videojuego . "\n";
-        echo "descripcion_videojuego: " . $descripcion_videojuego . "\n";
-        echo "url_portada: " . $url_portada . "\n";
-
         try {
-            // Preparar la consulta SQL para actualizar el videojuego
             $query_actualizar_videojuego = $base_datos->prepare("UPDATE videojuego SET nombre_videojuego=:nombre, descripcion_videojuego=:descripcion, url_portada=:portada WHERE id_juego=:id");
             $query_actualizar_videojuego->bindParam(":nombre", $nombre_videojuego);
             $query_actualizar_videojuego->bindParam(":descripcion", $descripcion_videojuego);
             $query_actualizar_videojuego->bindParam(":portada", $url_portada);
             $query_actualizar_videojuego->bindParam(":id", $id_juego);
 
-            // Ejecutar la consulta
             $ejecucion = $query_actualizar_videojuego->execute();
 
-            // Verificar si la actualización fue exitosa
             if($ejecucion) {
-                // Verificar el número de filas afectadas
                 $filas_afectadas = $query_actualizar_videojuego->rowCount();
                 if ($filas_afectadas > 0) {
                     $respuesta = [
@@ -53,7 +42,6 @@
             ];
         }
 
-        // Devolver la respuesta en formato JSON
         echo json_encode($respuesta);
     } else {
         $respuesta = [
