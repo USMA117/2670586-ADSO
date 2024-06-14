ab6ebfca4921ad9e64e4c61e528cb8a19505bfcd
Sebastian Usma <johan58usma@gmail.com>
2024-06-13 23:32:19 -0500

    [Diseño] Icono de la pagina

--- 13-videojuegosAPI/API/obtenerCategorias.php
+++ 13-videojuegosAPI/API/obtenerCategorias.php
@@ -4,7 +4,7 @@
     $consulta = $base_datos->query("SELECT * FROM categoria");
     $respuesta = $consulta->fetchALL(PDO::FETCH_ASSOC);
 
-    $respuesta = mb_convert_encoding($respuesta,'UTF-8','iso-8859-1');
+    $respuesta = mb_convert_encoding($respuesta,'UTF-8','ISO-8859-1');
     
     echo json_encode($respuesta);
 ?> 
